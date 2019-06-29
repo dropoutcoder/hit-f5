16aae8fb2cb30310111a51e7221f9de6ef421bbe
Petr Šrámek <petr.sramek@outlook.cz>
2019-06-29 07:52:34 +0200

    Revert "Delete Program.cs"
    This reverts commit 0a1ad907c09b1ae94ba8f57d69894c831a08c832.

---
+++ src/hitf5.Custom/Program.cs
@@ -0,0 +1,28 @@
+﻿using HitF5.Factorial;
+using System;
+using System.Threading.Tasks;
+
+namespace HitF5.Custom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = null;
+            var calculator = new FactorialCalculator();
+
+            do
+            {
+                if (Int32.TryParse(line, out var value))
+                {
+                    var result = calculator.Calculate(value);
+
+                    Console.WriteLine($"Faktoriál hodnoty {value} je {result}!");
+                }
+
+                Console.WriteLine($"Zadejte hodnotu pro výpočet faktoriálu:");
+                line = Console.ReadLine();
+            } while (line.ToLower() != "y");
+        }
+    }
+}
