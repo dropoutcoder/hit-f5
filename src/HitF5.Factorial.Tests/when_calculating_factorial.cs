4eac30dc20a0b33700b277a3dcd2a9db30a311c3
xhafan <xhafan@gmail.com>
2022-10-31 12:37:50 +0100

    Added missing tests for FactorialCalculator

---
+++ src/HitF5.Factorial.Tests/when_calculating_factorial.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace HitF5.Factorial.Tests
+{
+    [TestFixture]
+    public class when_calculating_factorial
+    {
+        [TestCase(2, 2)]
+        [TestCase(3, 6)]
+        [TestCase(4, 24)]
+        public void factorial_is_calculated_correctly(int value, int expectedFactorialValue)
+        {
+            var factorial = new FactorialCalculator().Calculate(value);
+
+            factorial.ShouldBe(expectedFactorialValue);
+        }
+    }
+}
